38949fef3b0c19ddee1b57bc890f76612938d2a2
yunus <yunus.karabulut1@gmail.com>
2019-04-21 17:41:15 +0300

    mapper for task8

---
+++ J0.mapper_question_id_abs_parent_id.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python                                                                                                                                                       
+                                                                                                                                                                        
+import sys, csv, string                                                                                                                                                 
+                                                                                                                                                                        
+reader = csv.reader(sys.stdin, delimiter = '\t', quotechar='"')                                                                                                         
+reader.next()                                                                                                                                                           
+                                                                                                                                                                        
+for line in reader:                                                                                                                                                     
+                                                                                                                                                                        
+    if len(line)!=19:                                                                                                                                                   
+        continue                                                                                                                                                        
+    else:                                                                                                                                                               
+        node_type = line[5]                                                                                                                                             
+        node_id = line[0]                                                                                                                                               
+        abs_parent_id = line[7]                                                                                                                                         
+                                                                                                                                                                        
+        if node_type == 'question':                                                                                                                                     
+            print '{0}\tA'.format(node_id)                                                                                                                              
+        else:                                                                                                                                                           
+            print '{0}\tB'.format(abs_parent_id)                                                                                                                        
+                                                   
